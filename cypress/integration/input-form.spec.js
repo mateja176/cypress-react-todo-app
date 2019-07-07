68a2e7144af3b47a156c41382b671fb021909d75
Mateja Petrovic <mateja176@gmail.com>
2019-07-07 19:59:37 +0200

    refactor(cypress): adds baseUrl in cypress.json

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,16 +1,14 @@
-const url = 'http://localhost:3030';
-
 const todoText = 'Buy Milk';
 
 describe('Input form', () => {
-  it('focuses input on load', () => {
-    cy.visit(url);
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
+  it('focuses input on load', () => {
     cy.focused().should('have.class', 'new-todo');
   });
   it.only('accepts input', () => {
-    cy.visit(url);
-
     cy.get('.new-todo')
       .type(todoText)
       .should('have.value', todoText);
