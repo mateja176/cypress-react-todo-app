eee7f26048705f44fa3da5dc5f55794e994fa7af
Mateja Petrovic <mateja176@gmail.com>
2019-07-07 19:14:41 +0200

    feat(cypress): adds `accepts input` test

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,7 +1,18 @@
+const url = 'http://localhost:3030';
+
+const todoText = 'Buy Milk';
+
 describe('Input form', () => {
   it('focuses input on load', () => {
-    cy.visit('http://localhost:3030');
+    cy.visit(url);
 
     cy.focused().should('have.class', 'new-todo');
+  });
+  it.only('accepts input', () => {
+    cy.visit(url);
+
+    cy.get('.new-todo')
+      .type(todoText)
+      .should('have.value', todoText);
   });
 });
